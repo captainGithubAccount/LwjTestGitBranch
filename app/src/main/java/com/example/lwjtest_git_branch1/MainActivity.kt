997f00d0d3f7daf49faab9e4499901b2735f1b98
lwj <395901976@qq.com>
2022-09-20 11:03:44 +0800

    branch3分支修改

--- app/src/main/java/com/example/lwjtest_git_branch1/MainActivity.kt
+++ app/src/main/java/com/example/lwjtest_git_branch1/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //来自branch3分支修改的
     }
 }
